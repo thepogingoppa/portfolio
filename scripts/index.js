71df1989dbfda668758e0ab6d894ac30103efa72
Jozua Patalan <jozuapatalan@gmail.com>
2025-06-08 20:57:51 -0400

    added js files

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const typingElement = document.querySelector(".typing");
+const speed = 50;
+const positions = [
+  "UX Designer",
+  "Frontend Developer",
+  "Backend Developer",
+  "Software Engineer",
+];
---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+let navigation_links = document.querySelectorAll(".website-nav a");
+navigation_links[0].classList.add("selected");
