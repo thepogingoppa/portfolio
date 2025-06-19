af0684ed6bb6df4dfc5c433ea6d55d1fbe94ec18
Jozua Patalan <jozuapatalan@gmail.com>
2025-06-18 21:12:26 -0400

    highlights current page
    added a functionality such that the website will highlight the active page

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,27 @@
-let navigation_links = document.querySelectorAll(".website-nav a");
-navigation_links[0].classList.add("selected");
+function currentPageHighlighter()
+{
+    const navigationElement = document.querySelectorAll('nav > div');
+    let currentPage;
+    if (document.URL.includes("index.html"))
+    {
+        currentPage = navigationElement[0];
+    }
+    else if (document.URL.includes("about.html"))
+    {
+        currentPage = navigationElement[1];
+    }
+    else if (document.URL.includes("works.html"))
+    {
+        currentPage = navigationElement[2];
+    }
+    currentPage.classList.add('selected');
+}
+
+
+
+function main()
+{
+    currentPageHighlighter();
+}
+
+main();
